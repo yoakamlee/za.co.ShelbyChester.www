3310d826ab534863e040b517ae1fa641d87cf092
yoakamlee <yoakamleesoupen@gmail.com>
2022-05-25 19:34:59 +0200

    Added ContainerCategory viewmodel

---
+++ ShelbyChester.Core/ViewModels/ContainerCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShelbyChester.Core.ViewModels
+{
+    public class ContainerCategory
+    {
+        public string Id { get; set; }
+        public string Category { get; set; }
+
+        public int baseWeight { get; set; }
+
+        public int size { get; set; }
+
+        public ContainerCategory()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
