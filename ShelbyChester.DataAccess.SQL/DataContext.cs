dc8466b27dc4d37efa4f36597fb4ca59ea368480
Yoakam <yoakamleesoupen@gmail.com>
2022-06-01 18:55:42 +0200

    Added database and migrations, added entityframework to DataAccess.SQL and changed the attached file and catalog in web.config

---
+++ ShelbyChester.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using ShelbyChester.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShelbyChester.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<ClientPreAdvice> clientPreAdvices { get; set; }
+        public DbSet<ContainerCategory> containerCategories { get; set; }
+        public DbSet<FreightQuotation> freightQuotations { get; set; }
+        public DbSet<WarehouseStorage> warehouseStorages { get; set; }
+    }
+}
---
+++ ShelbyChester.DataAccess.SQL/Migrations/202206011653296_Initial.cs
@@ -0,0 +1,80 @@
+﻿namespace ShelbyChester.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ClientPreAdvices",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Customer_Name = c.String(nullable: false),
+                        Customer_Surname = c.String(nullable: false),
+                        Customer_Address = c.String(nullable: false),
+                        Customer_Email = c.String(nullable: false),
+                        Customer_CellNum = c.String(),
+                        NumberOfItems = c.Int(nullable: false),
+                        WeightOfItems = c.Int(nullable: false),
+                        SizePerItem = c.String(),
+                        Client_Description = c.String(maxLength: 500),
+                        Country = c.Int(nullable: false),
+                        ContainerType = c.String(nullable: false),
+                        Province = c.Int(nullable: false),
+                        CreatedAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.ContainerCategories",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        ContainerName = c.String(),
+                        ContainerWeight = c.Int(nullable: false),
+                        ContainerSize = c.Int(nullable: false),
+                        CreatedAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.FreightQuotations",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        name = c.String(nullable: false),
+                        Price = c.Double(nullable: false),
+                        quantity = c.Int(nullable: false),
+                        Weight = c.String(nullable: false),
+                        size = c.String(nullable: false),
+                        basicprice = c.Double(nullable: false),
+                        totalPrice = c.Double(nullable: false),
+                        CreatedAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.WarehouseStorages",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(),
+                        Location = c.String(),
+                        CreatedAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.WarehouseStorages");
+            DropTable("dbo.FreightQuotations");
+            DropTable("dbo.ContainerCategories");
+            DropTable("dbo.ClientPreAdvices");
+        }
+    }
+}
