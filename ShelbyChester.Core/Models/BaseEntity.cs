284e08b4d02929e84776c116c623d17198012147
Yoakam <yoakamleesoupen@gmail.com>
2022-06-02 17:34:39 +0200

    Added display data annotaiton to BaseEntity CreatedAt

--- ShelbyChester.Core/Models/BaseEntity.cs
+++ ShelbyChester.Core/Models/BaseEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace ShelbyChester.Core.Models
     public abstract class BaseEntity
     {
         public string Id { get; set; }
+
+        [Display(Name ="Last edited")]
         public DateTime CreatedAt { get; set; }
 
         public BaseEntity()
